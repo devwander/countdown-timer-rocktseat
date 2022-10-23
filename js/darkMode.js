1c7488990d5b5d0246daaddaaf0b75a80940427d
devwander <josewanderson173@gmail.com>
2022-10-23 15:06:51 -0300

    adding dark mode button

---
+++ js/darkMode.js
@@ -0,0 +1,36 @@
+document.body.style = "background-color: var(--white); transition: 0.5s;"
+
+let theme = "light";
+
+const body = document.body
+const container = document.querySelector(".theme-container");
+const themeIcon = document.querySelector(".theme-icon");
+
+container.addEventListener("click", setTheme);
+
+function setTheme() {
+  switch (theme) {
+    case "dark":
+      setLight();
+      theme = "light";
+      break;
+    case "light":
+      setDark();
+      theme = "dark";
+      break;
+  }
+}
+
+function setLight() {
+  body.style.background = "white";
+  body.style.transition = "0.5s"
+  themeIcon.src = "../assets/sun.svg";
+  container.style.background = "var(--sun-button)"
+}
+
+function setDark() {
+  body.style.background = "var(--background-dark)";
+  body.style.transition = "0.5s"
+  themeIcon.src = "../assets/moon.svg";
+  container.style.background = "var(--moon-button)"
+}
